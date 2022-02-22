1f02634a5c10ab348dbf1149c010c592c23a082a
Robi <babirak.robert0521@gmail.com>
2022-02-22 12:46:28 +0100

    első feladat java

--- java.js
+++ java.js
@@ -1,4 +1,14 @@
 function kiertekeles()
 {
-
+    var pontok = 0;
+    if(document.getElementById("balaton").checked==true){
+        pontok+=1;
+    }
+    if(document.getElementById("oregto").checked==true){
+        pontok+=0;
+    }
+    if(document.getElementById("tiszato").checked==true){
+        pontok+=0;
+    }
+    
 }
